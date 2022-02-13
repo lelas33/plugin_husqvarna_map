dac43fb8d3d3fee8a39450c17682790ca5a2f2ac
lelas <laurent.jardin@free.fr>
2022-02-13 17:38:53 +0100

    Mise à jour pour Jeedom V4.2

--- 3rdparty/sample.php
+++ 3rdparty/sample.php
@@ -1,7 +1,10 @@
 <?php
 require_once("husqvarna_map_api.class.php");
+define("MOWER_ID", "xxxxxxxxx-yyyyyyyyy");
+
 $account = "xx.yy@zz.fr";
 $passwd = "pp";
+
 $session_husqvarna = new husqvarna_map_api();
 $session_husqvarna->login($account, $passwd);
 
@@ -10,29 +13,29 @@ $session_husqvarna->login($account, $passwd);
 // print("\n");
 
 // print("control :\n");
-// var_dump($session_husqvarna->control("191510477-191232162", 'START'));
+// var_dump($session_husqvarna->control(MOWER_ID, 'START'));
 // print("\n");
 
 print("get_status :\n");
-var_dump($session_husqvarna->get_status("191510477-191232162"));
+var_dump($session_husqvarna->get_status(MOWER_ID));
 print("\n");
 
 // print("get_geofence :<pre>");
-// var_dump($session_husqvarna->get_geofence("191510477-191232162"));
+// var_dump($session_husqvarna->get_geofence(MOWER_ID));
 // print("</pre>");
 
 // print("get_settings :\n");
-// var_dump($session_husqvarna->get_settings("191510477-191232162"));
+// var_dump($session_husqvarna->get_settings(MOWER_ID));
 // print("\n");
 
 print("get_statistics :\n");
-var_dump($session_husqvarna->get_statistics_app("191510477-191232162"));
+var_dump($session_husqvarna->get_statistics_app(MOWER_ID));
 print("\n");
 
 print("get_timers :\n");
-var_dump($session_husqvarna->get_timers("191510477-191232162"));
+var_dump($session_husqvarna->get_timers(MOWER_ID));
 print("\n");
-var_dump($session_husqvarna->get_timers_app("191510477-191232162"));
+var_dump($session_husqvarna->get_timers_app(MOWER_ID));
 print("\n");
 
 $session_husqvarna->logout();
